d791724f7b4cf4e970daf402277b41fe13ce0d86
Guilherme Natal de Mello <guinatal.mello@gmail.com>
2017-11-20 12:05:06 +0000

    second commit

---
+++ Kefron/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Kefron._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="form-group">
+        <asp:TextBox ID="txtName" runat="server" MaxLength="50" placeholder="Name"></asp:TextBox>
+    </div>
+    <div class="form-group">
+        <asp:TextBox ID="txtAddress" runat="server" MaxLength="50" placeholder="Address"></asp:TextBox>
+    </div>
+    <div class="form-group">
+        <asp:TextBox ID="txtDateOfBirth" runat="server" MaxLength="50" placeholder="Date Of Birth" TextMode="Date"></asp:TextBox>
+    </div>
+    <div class="form-group">
+        <asp:TextBox ID="txtEmail" runat="server" MaxLength="50" placeholder="Email" TextMode="Email"></asp:TextBox>
+    </div>
+    <div class="form-group">
+        <asp:ListBox ID="listMenu" runat="server" SelectionMode="Multiple"></asp:ListBox>
+    </div>
+    <div class="form-group">
+        <asp:Button ID="btnSend" runat="server" Text="Send" OnClick="btnSend_Click" />
+    </div>
+
+</asp:Content>
---
+++ Kefron/Default.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kefron
+{
+    public partial class _Default : Page
+    {
+        private static void OpenSqlConnection()
+        {
+            string connectionString = GetConnectionString();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                Console.WriteLine("State: {0}", connection.State);
+                Console.WriteLine("ConnectionString: {0}",
+                    connection.ConnectionString);
+            }
+        }
+
+        static private string GetConnectionString()
+        {
+            // To avoid storing the connection string in your code, 
+            // you can retrieve it from a configuration file, using the 
+            // System.Configuration.ConfigurationSettings.AppSettings property 
+            return "Data Source=(local);Initial Catalog=AdventureWorks;"
+                + "Integrated Security=SSPI;";
+        }
+
+        SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyfFrstDataBaseConnectionString"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            myConnection.Open();
+
+            string query = "select * from [dbo].[Menu]";
+
+            // use a SqlAdapter to execute the query
+            using (SqlDataAdapter a = new SqlDataAdapter("SELECT * FROM Menu", myConnection))
+            {
+                // fill a data table
+                var t = new DataTable();
+                a.Fill(t);
+
+                // Bind the table to the list box
+                listMenu.DataTextField = "name";
+                listMenu.DataValueField = "id";
+                listMenu.DataSource = t;
+                listMenu.DataBind();
+            }
+
+            myConnection.Close();
+
+        }
+
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+
+            string name = txtName.Text;
+            string address = txtAddress.Text;
+            string dateOfBirth = txtDateOfBirth.Text;
+            string email = txtEmail.Text;
+
+            myConnection.Open();
+
+            // Customer
+            string query = "Insert into [dbo].[Customer] (name, address, date_of_birth, email) Values (@name, @address, @date_of_birth, @email)";
+            SqlCommand insertCommand = new SqlCommand(query, myConnection);
+            insertCommand.Parameters.AddWithValue("@name", name);
+            insertCommand.Parameters.AddWithValue("@address", address);
+            insertCommand.Parameters.AddWithValue("@date_of_birth", dateOfBirth);
+            insertCommand.Parameters.AddWithValue("@email", email);
+            insertCommand.Parameters.Add("@id", SqlDbType.Int, 4).Direction = ParameterDirection.Output;
+            int id_customer = insertCommand.ExecuteNonQuery();
+
+            // Customer X Menu
+            foreach (int i in listMenu.GetSelectedIndices())
+            {
+
+                int id_menu = int.Parse(listMenu.Items[i].Value);
+
+                query = "Insert into [dbo].[Customer_X_Menu] (id_menu, id_customer) Values (@id_menu, @id_customer)";
+                insertCommand = new SqlCommand(query, myConnection);
+                insertCommand.Parameters.AddWithValue("@id_menu", id_menu);
+                insertCommand.Parameters.AddWithValue("@id_customer", id_customer);
+                insertCommand.ExecuteNonQuery();
+
+            }
+
+            myConnection.Close();
+
+
+        }
+    }
+}
